a5e3aa688c592f6dc4fb13c5fcba2a24641aa327
Ultracat39 <ultracat39@gmail.com>
2025-02-02 10:52:32 -0600

    still an idiot

--- pages/main.js
+++ pages/main.js
@@ -1,4 +1,4 @@
-import { xmlGetHttpRequest } from "./xml-get-http-request"
+import { xmlGetHttpRequest } from "./xml-get-http-request.js"
 
 const gistUrl = 'https://gist.github.com/sturmenta/df1c9da1f219c88e996e48f19d57acd3';
 
