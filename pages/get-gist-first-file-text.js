7af152a480cc7a8a750b1583d810d3e1291ac866
Ultracat39 <ultracat39@gmail.com>
2025-02-02 17:42:32 -0600

    skibidi idiot

--- pages/get-gist-first-file-text.js
+++ pages/get-gist-first-file-text.js
@@ -3,13 +3,14 @@ export const getGistFirstFileText = async url => {
     try {
       const { data: gistData, error: gistError } = await xmlGetHttpRequest(url)
       if (gistError) res({ error: gistError })
-
       // ─────────────────────────────────────────────────────
       // 1. find raw link
       const html = JSON.parse(gistData).div
+                                                                console.log(html)
       let rawLink = ""
       html.split('href="').forEach(text => {
         const link = text.split('"')[0]
+                                                                console.log(link)
         if (link.includes("/raw/")) rawLink = link
       })
       if (!rawLink) res({ error: "raw link not found" })
@@ -22,6 +23,7 @@ export const getGistFirstFileText = async url => {
       // ─────────────────────────────────────────────────────
 
       res({ data: rawData })
+                                                                console.log(data)
     } catch (e) {
       res({ error: String(e) })
     }
