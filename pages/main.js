aeac8c55d618007d4a42a641c9436e45be184a3a
Ultracat39 <ultracat39@gmail.com>
2025-02-02 18:01:00 -0600

    skibidi idiot +2

--- pages/main.js
+++ pages/main.js
@@ -1,5 +1,5 @@
-import { xmlGetHttpRequest } from "./xml-get-http-request.js"
-import {getGistFirstFileText} from "./get-gist-first-file-text.js"
+//import { xmlGetHttpRequest } from "./xml-get-http-request.js"
+//import {getGistFirstFileText} from "./get-gist-first-file-text.js"
 
 const gistUrl = 'https://gist.github.com/sturmenta/df1c9da1f219c88e996e48f19d57acd3';
 
