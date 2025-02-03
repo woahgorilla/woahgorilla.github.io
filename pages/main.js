f3b3314d1b5385454c268bd1b25a766311b2a3ce
Ultracat39 <ultracat39@gmail.com>
2025-02-02 18:05:46 -0600

    skibidi idiot +3

--- pages/main.js
+++ pages/main.js
@@ -1,11 +1,7 @@
 //import { xmlGetHttpRequest } from "./xml-get-http-request.js"
 //import {getGistFirstFileText} from "./get-gist-first-file-text.js"
 
-const gistUrl = 'https://gist.github.com/sturmenta/df1c9da1f219c88e996e48f19d57acd3';
 
-const {data, error} = /*await*/ getGistFirstFileText(`${gistUrl}.json`);
-
-console.log(data);
 
 
 const getGistFirstFileText = async url => {
@@ -59,3 +55,12 @@ const getGistFirstFileText = async url => {
     })
   }
   
+
+
+
+
+  const gistUrl = 'https://gist.github.com/sturmenta/df1c9da1f219c88e996e48f19d57acd3';
+
+const {data, error} = /*await*/ getGistFirstFileText(`${gistUrl}.json`);
+
+console.log(data);
