e497d6278dd2bb0b67d4c36e7d739b7874073dba
Ultracat39 <ultracat39@gmail.com>
2025-03-09 17:53:27 -0500

    some test stuff

---
+++ play/extras/cloaker.js
@@ -0,0 +1,21 @@
+//simple-ass tab cloaker inspired by the one on 3kh0.github.io
+//made with ¬ᴗ¬ (and inspiration from 3kh0.github.io) by Ultracat39
+//to set this stuff, go to woahgorilla.github.io/settings.html
+console.log('hi')
+const ls = window.localStorage
+var icon = ls.getItem('!INTERNAL!icon')
+var title = ls.getItem('!INTERNAL!title')
+if (icon) {
+  var link = document.querySelector("link[rel~='icon']");
+  if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.head.appendChild(link);
+  }
+link.href = 'icon';
+}
+if (title) {
+  console.log(title)
+  document.title = title
+}
+console.log('tab cloaker made with ¬ᴗ¬ by Ultracat39')
